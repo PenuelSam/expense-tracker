24676ee97f9be26ed74154b64e3e784a55c2e4e0
penuel <penuelsamuel54@gmail.com>
2022-09-16 21:32:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import { Header } from './components/Header';
+import IncomeExpanses from './components/IncomeExpanses';
+import TransactionList from './components/TransactionList';
+import {GlobalProvider} from './context/GlobalState';
+
+function App() {
+  return (
+      <GlobalProvider>
+        <Header />
+        <div className='container'>
+          <Balance />
+          <IncomeExpanses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    
+  );
+}
+
+export default App;
